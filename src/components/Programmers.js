f2227a10f132c2cc233fe12c13c0e80143f0299a
Rafael Molina <seissaysseis@users.noreply.github.com>
2020-09-09 20:26:52 -0600

    DOOONE

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -37,7 +37,8 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    
+    let person = list.find(item => featured === item.id);
+    return person.name
   };
 
   const style = {
